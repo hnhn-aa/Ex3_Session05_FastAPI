51fa3ae1172bf777f0a9886c52f778b578734047
hnhn-aa <baitapvenhacuanhan@gmail.com>
2026-06-30 21:02:08 +0700

    Add files via upload

---
+++ Ex3/ex3.py
@@ -0,0 +1,16 @@
+students = [
+    {"id": 1, "name": "Nguyen Van A"},
+    {"id": 2, "name": "Tran Thi B"},
+    {"id": 3, "name": "Le Van C"}
+]
+
+courses = [
+    {"id": 1, "name": "FastAPI Basic", "capacity": 2},
+    {"id": 2, "name": "Python OOP", "capacity": 2}
+]
+
+registrations = [
+    {"id": 1, "student_id": 1, "course_id": 1},
+    {"id": 2, "student_id": 2, "course_id": 1}
+]
+
